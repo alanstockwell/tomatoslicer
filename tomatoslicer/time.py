5bec0aef470e9d6f949b255f8a32074cb45c05f4
Lewis Munson <lewis@decodeit.co.nz>
2023-09-22 10:32:10 +1200

    fix weirdness around TimeSlice.iter over daylight savings time

--- tomatoslicer/time.py
+++ tomatoslicer/time.py
@@ -224,14 +224,14 @@ class TimeSlice(object):
         current_time_slice = TimeSlice(self.start, end=self.start + interval)
 
         while current_time_slice.end - one_microsecond <= self.end:
+            yield TimeSlice(current_time_slice.start, end=current_time_slice.end - one_microsecond)
+
+            current_time_slice += interval
+
             if current_time_slice.spans_dst_start:
                 current_time_slice.end -= current_time_slice.end.dst()
             elif current_time_slice.spans_dst_end:
                 current_time_slice.end += current_time_slice.end.dst()
-
-            yield TimeSlice(current_time_slice.start, end=current_time_slice.end - one_microsecond)
-
-            current_time_slice += interval
 
     def iter_days(self, step=1):
         return self.iter(relativedelta(days=step))
